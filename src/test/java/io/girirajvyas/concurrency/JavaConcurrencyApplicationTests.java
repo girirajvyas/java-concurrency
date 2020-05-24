a625c3b6ef8489d789307beb158ad23c71407d91
Giriraj Vyas <girirajvyas21@gmail.com>
2020-05-24 14:10:08 +0530

    Repository initialized

---
+++ src/test/java/io/girirajvyas/concurrency/JavaConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.girirajvyas.concurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaConcurrencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
